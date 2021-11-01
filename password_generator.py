613f4627a5eba1135487c0392c8ecf9982c21456
Gabriel M <gabomonher2.0@gmail.com>
2021-11-01 11:23:46 -0500

    Password generator 1.0

---
+++ password_generator.py
@@ -0,0 +1,26 @@
+import random
+
+
+def randomPassword():
+    UPPERCASE = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z']
+    LOWERCASE = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
+    NUMS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    CHARS = ['*', '+', '-', '/', '@', '_', '?', '!', '[', '{', '(', ')', '}', ']', ',', ';', '.', '>', '<', '~', '°', '^', '&', '$', '#', '"']
+    characters=UPPERCASE+LOWERCASE+NUMS+CHARS
+    password=[]
+    for i in range(15):
+        random_character=random.choice(characters)
+        password.append(random_character)
+    password="".join(password)
+    return password
+
+
+def run():
+    password=randomPassword()
+    print("""Hello 😄👋🏻
+I'm your password generator.
+Your password is: {}""".format(password))
+
+
+if __name__=="__main__":
+    run()
